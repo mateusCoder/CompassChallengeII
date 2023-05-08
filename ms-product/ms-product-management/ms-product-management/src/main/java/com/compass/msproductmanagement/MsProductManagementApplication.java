4b240e67bcc8a3962c670c2dbbd75ded38e8e6c4
mateusCoder <mateus.moraes0507@gmail.com>
2023-05-08 17:54:19 -0300

    Project Created

---
+++ ms-product/ms-product-management/ms-product-management/src/main/java/com/compass/msproductmanagement/MsProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.compass.msproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductManagementApplication.class, args);
+	}
+
+}
---
+++ ms-product/ms-product-management/ms-product-management/src/test/java/com/compass/msproductmanagement/MsProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.msproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
